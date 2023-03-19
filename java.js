0f6ce599bae336e706cb8ab456acf6d692abfe50
venlaa <127877366+venlaa@users.noreply.github.com>
2023-03-19 18:20:27 +0200

    f

---
+++ java.js
@@ -0,0 +1,29 @@
+
+
+function fruittester() {
+    let number = Number(document.getElementById("number").value);
+    
+    if(number == 1) {
+        document.getElementById("result").innerHTML = "Apple"
+    } else if (number == 2) {
+        document.getElementById("result").innerHTML = "Watermelon"
+    } else if (number ==3) {
+        document.getElementById("result").innerHTML = "Kiwi"
+    } else if (number ==4) {
+        document.getElementById("result").innerHTML = "Pineapple"
+    } else if (number ==5) {
+        document.getElementById("result").innerHTML = "Pear"
+    } else if (number ==6) {
+        document.getElementById("result").innerHTML = "Honeydew"
+    } else if (number ==7) {
+        document.getElementById("result").innerHTML = "Banana"
+    } else if (number ==8) {
+        document.getElementById("result").innerHTML = "Grapefruit"
+    } else if (number ==9) {
+        document.getElementById("result").innerHTML = "Mandarin"
+    }
+}
+
+  
+
+
